094f7acfaa30de5f4abb2d01867530db31d8a0ed
ㅤ𝛎 𝜄 ᥉ ᥴ᥆ ᥉ 𝜄 𝗍 𝛄 ✞ㅤ <166417113+viscity@users.noreply.github.com>
2025-12-16 16:06:13 +0530

    Update support channel and chat URLs

--- config.py
+++ config.py
@@ -44,8 +44,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/life_update_ll")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/aboutva7ce")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/aboutva7ce")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/LOVE_COMMUNITY_l")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
